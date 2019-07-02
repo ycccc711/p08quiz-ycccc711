7b1aee1092f89c02a02056e307c9db3511948630
Jason Lim <jason_lim@rp.edu.sg>
2019-07-02 13:54:44 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/soi/c346_p08quiz/MainActivity.java
@@ -0,0 +1,13 @@
+package sg.edu.rp.soi.c346_p08quiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
